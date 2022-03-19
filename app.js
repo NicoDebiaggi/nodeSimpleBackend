7c36b960072a8059c5f02ab68d0043935c9f8347
Nicolas Enrique Debiaggi <nicodebiaggi@gmail.com>
2022-03-19 00:35:32 -0300

    handlebars added

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 import express from 'express'
 import productRouter from './src/Routes/products.routes.js'
+import { engine } from 'express-handlebars'
+import { products, getMaxId } from './src/Controllers/products.controllers.js'
+import path from 'path'
 
 const app = express()
 const PORT = 8080
+const __dirname = path.resolve()
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} 🔥🔥🔥`)
 })
 app.use(express.json())
-app.use('/', productRouter)
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('hbs', engine({
+  extname: '.hbs',
+  defaultLayout: 'layout.hbs',
+  layoutsDir: __dirname + '/src/Views/Layouts',
+  partialsDir: __dirname + '/src/Views/Partials'
+}))
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/src/Views/Layouts')
+
+app.get('/products', (req, res) => {
+  res.render('layout', { products })
+})
+app.post('/products', (req, res) => {
+  const newProduct = {
+    title: req.body.title,
+    price: req.body.price,
+    thumbnail: req.body.thumbnail,
+    id: getMaxId() + 1
+  }
+  products.push(newProduct)
+  res.redirect('/products')
+})
+
+app.use('/api', productRouter)
 
 app.use(function (err, req, res, next) {
   console.log(err)
